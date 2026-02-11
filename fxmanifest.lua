8dee43957857ec872769886fd3c5e28a47cd590f
ChiLLLix-hub <chilllix.me@gmail.com>
2026-02-11 14:56:48 +0800

    Add fxmanifest for QB-Core Mini Character Ped Spawner

---
+++ fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'YourName'
+description 'QB-Core Mini Character Ped Spawner'
+version '1.0.0'
+
+client_scripts {
+    'client/main.lua'
+}
+
+dependencies {
+    'qb-core'
+}
